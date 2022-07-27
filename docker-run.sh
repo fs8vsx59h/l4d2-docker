a5d87d3b6a71833c8696f7bee12b149c6fe00156
fs8vsx59h <106460965+fs8vsx59h@users.noreply.github.com>
2022-07-27 20:04:40 +0800

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 docker run -d \
 --network=host \
--v /root/l4d2-sources/addons:/home/steam/left4dead2/left4dead2/addons \
--v /root/l4d2-sources/server.cfg:/home/steam/left4dead2/left4dead2/cfg/server.cfg \
-left4dead2:latest /home/steam/left4dead2/srcds_run -console -game left4dead2 +port 27020 +maxplayers 8 +exec server.cfg +map electric_Power_School_m1 -nomaster
+-v /root/l4d2/addons:/home/steam/left4dead2/left4dead2/addons \
+-v /root/l4d2/server.cfg:/home/steam/left4dead2/left4dead2/cfg/server.cfg \
+poormancontainer/l4d2simple:latest /home/steam/left4dead2/srcds_run -console -game left4dead2 +port 27001 +maxplayers 8 +exec server.cfg +map zc_m1 -nomaster
